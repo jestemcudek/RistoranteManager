83a4315cd594cd30a7abf81640533cc2e1a06a97
jestemcudo <damian.cudek@gmail.com>
2016-04-23 17:38:52 +0200

    Added files via upload

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RistoranteManager
+{
+    class Product
+    {
+        string name;
+        double minimum_Quantity;
+        double current_Quantity;
+        Supplier supplier;
+
+    }
+}
